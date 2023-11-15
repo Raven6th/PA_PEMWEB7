b6e76945be03c0010080f1afb7cef75b98b7bb30
Raven6th <145556185+Raven6th@users.noreply.github.com>
2023-11-15 16:35:58 +0800

    Update keranjang.php

--- user/keranjang.php
+++ user/keranjang.php
@@ -15,7 +15,7 @@ if ($_SESSION["akses"] !== "user") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Keranjang Belanja</title>
+    <title>Document</title>
     <link rel="stylesheet" href="../style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -29,7 +29,6 @@ if ($_SESSION["akses"] !== "user") {
             background-color: #f9f9f9;
             color: #333;
         }
-
         header {
             background-color: #333;
             padding: 15px;
@@ -42,18 +41,15 @@ if ($_SESSION["akses"] !== "user") {
             top: 0;
             z-index: 1000;
         }
-
         .navbar {
             list-style-type: none;
             margin: 0;
             padding: 0;
             display: flex;
         }
-
         .navbar li {
             margin-right: 20px;
         }
-
         .navbar a {
             text-decoration: none;
             color: white;
@@ -61,16 +57,13 @@ if ($_SESSION["akses"] !== "user") {
             font-size: 16px;
             transition: color 0.3s;
         }
-
         .navbar a:hover {
             color: #FF702a;
         }
-
         .home {
             margin-top: 80px;
             text-align: center;
         }
-
         .home-text {
             max-width: 800px;
             margin: 0 auto;
@@ -79,7 +72,6 @@ if ($_SESSION["akses"] !== "user") {
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             border-radius: 10px;
         }
-
         table {
             width: 100%;
             margin-top: 20px;
@@ -88,21 +80,17 @@ if ($_SESSION["akses"] !== "user") {
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             border-radius: 10px;
         }
-
         th, td {
             border: 1px solid #ddd;
             padding: 10px;
             text-align: center;
         }
-
         th {
             background-color: #f2f2f2;
         }
-
         tr:hover {
             background-color: #E5E5E5;
         }
-
         a.button {
             display: inline-block;
             padding: 10px 20px;
@@ -113,7 +101,6 @@ if ($_SESSION["akses"] !== "user") {
             border-radius: 5px;
             transition: background-color 0.3s;
         }
-
         a.button:hover {
             background-color: #555;
         }
@@ -127,117 +114,13 @@ if ($_SESSION["akses"] !== "user") {
 
         <ul class="navbar">
             <body class="light-mode">
-                <li <?php if($_SESSION["akses"] === "user" || $_SESSION["akses"] === "none"){ echo 'style="display: none;"';}?>><a href="admin/tambah.php">Tambah</a></li>
-            <?php
-            if ($_SESSION["akses"] === "user" || $_SESSION["akses"] === "admin"){
-            echo 
-            '
-                <li><a id="toggle-mode">Mode Tema</a></li>
-            ';
-            }
-            ?>
-                <li><a href="../index.php">Home</a></li>
-                <li <?php if($_SESSION["akses"] === "user" || $_SESSION["akses"] === "admin"){ echo 'style="display: none;"';}?>><a href="login.php">Login</a></li>
-                <li <?php if($_SESSION["akses"] === "none"){ echo 'style="display: none;"';}?>><a href="logout.php">Logout</a></li>
-                <li><a href="../index.php#menu">Menu</a></li>
-                <li><a href="../index.php#services">Service</a></li>
-                <li><a href="../index.php#contact">Contact</a></li>
-        </ul> 
-    </header>
-
-    <section class="home" id="home">
-        <div class="home-text">
-            <h1>Keranjang Belanja</h1>
-            <hr>
-            <?php if (empty($_SESSION['keranjang'])) {
-                echo '
-                
-                <h2>Keranjang Kosong</h2>
-                ';
-            }
-            else 
-                echo '
-                <table>
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Produk</th>
-                            <th>Harga</th>
-                            <th>Jumlah</th>
-                            <th>Total Harga</th>
-                        </tr>
-                    </thead>
-                    <tbody>';
-                    $nomor = 1;
-                    foreach ($_SESSION['keranjang'] as $id_produk => $jumlah): ?>
-                    <?php 
-                    $fetch = $koneksi->query("SELECT * FROM produk WHERE id_produk ='$id_produk'");
-                    $ranjang = $fetch->fetch_assoc();
-                    $total = $ranjang["harga_produk"]*$jumlah;
-                    ?>
-                        <tr>
-                            <td><?php echo $nomor; ?></td>
-                            <td><?php echo $ranjang["nama_produk"];?></td>
-                            <td>$<?php echo number_format($ranjang["harga_produk"]);?></td>
-                            <td><?php echo $jumlah?></td>
-                            <td>$<?php echo number_format($total);?></td>
-                        </tr>
-                        <?php $nomor++; ?>
-                <?php endforeach; ?>
-                    </tbody>
-                </table>
-                <a href="../index.php" class="button">Kembali ke Halaman Utama</a>';
-        </div>
-    </section>
-</body>
-</html>
-<?php
-session_start();
-require '../koneksi.php';
-if (!isset($_SESSION["akses"])){
-    $_SESSION["akses"] = "none";
-}
-if ($_SESSION["akses"] !== "user") {
-    echo "<script>alert('Anda perlu login untuk melihat keranjang belanja')</script>";
-    header("Location: ../index.php");
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-
-    <header>
-        <a href="../index.php" class="logo">Chocolate</a>
-        <div class="fas fa-bars" id="menu-icon"></div>
-
-        <ul class="navbar">
-            <body class="light-mode">
             <li <?php if($_SESSION["akses"] === "user" || $_SESSION["akses"] === "none"){ echo 'style="display: none;"';}?>><a href="admin/tambah.php">Tambah</a></li>
-        <?php
-        if ($_SESSION["akses"] === "user" || $_SESSION["akses"] === "admin"){
-        echo 
-        '
-            <li><a id="toggle-mode">Mode Tema</a></li>
-        ';
-        }
-        ?>
-            <li><a href="#home">Home</a></li>
+            <li><a href="../index.php">Home</a></li>
             <li <?php if($_SESSION["akses"] === "user" || $_SESSION["akses"] === "admin"){ echo 'style="display: none;"';}?>><a href="login.php">Login</a></li>
             <li <?php if($_SESSION["akses"] === "none"){ echo 'style="display: none;"';}?>><a href="logout.php">Logout</a></li>
-            <li><a href="#menu">Menu</a></li>
-            <li><a href="#services">Service</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="../index.php">Menu</a></li>
+            <li><a href="../index.php">Service</a></li>
+            <li><a href="../index.php">Contact</a></li>
         </ul> 
     </header>
 
@@ -245,7 +128,7 @@ if ($_SESSION["akses"] !== "user") {
         <div class="home-textt">
             <h1>Keranjang</h1>
             <hr>
-            <?php if (empty($_SESSION['keranjang'])) {
+            <?php if (empty($_SESSION['keranjang']) || !isset($_SESSION['keranjang'])) {
                 echo '
                 
                 <h2>Keranjang Kosong</h2>
@@ -253,9 +136,9 @@ if ($_SESSION["akses"] !== "user") {
             }
             else {
                 echo '
-                // pembuat tabel
+                <!-- pembuat tabel --!>
                 <table class="table table-bordered">
-                // kepala tabel
+                <!-- kepala tabel --!>
                     <thead>
                         <tr>
                             <th>No</th>
@@ -263,9 +146,10 @@ if ($_SESSION["akses"] !== "user") {
                             <th>Harga</th>
                             <th>Jumlah</th>
                             <th>Total Harga</th>
+                            <th>Aksi</th>
                         </tr>
                     </thead>
-                // isi tabel
+                <!-- isi tabel --!>
                     <tbody>';
                     $nomor = 1;
                     foreach ($_SESSION['keranjang'] as $id_produk => $jumlah): ?>
@@ -281,6 +165,8 @@ if ($_SESSION["akses"] !== "user") {
                             <td>$<?php echo number_format($ranjang["harga_produk"]);?></td>
                             <td><?php echo $jumlah?></td>
                             <td>$<?php echo number_format($total);?></td>
+                            <!-- Hapus produk dari keranjang -->
+                            <td><a href="hapker.php?id=<?php echo $id_produk ?>">Hapus</a></td> 
                             
                         </tr>
                         <?php $nomor++; ?>
@@ -290,6 +176,8 @@ if ($_SESSION["akses"] !== "user") {
                         </tbody>
                     </table>';
                 } ?>
+                <a href="../index.php">Kembali Belanja</a>
+                <a href="checkout.php">Checkout</a>
         </div>
     </section>
 </body>
