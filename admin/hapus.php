ee57b3564c0f2d0d1b2c400937cfb2c1226607e7
Raven6th <145556185+Raven6th@users.noreply.github.com>
2023-11-15 14:05:00 +0800

    Update hapus.php

--- admin/hapus.php
+++ admin/hapus.php
@@ -1,6 +1,8 @@
 <?php
 require '../koneksi.php';
-
+if (!isset($_SESSION['akses']) || $_SESSION['akses'] !== 'admin') {
+    header('Location: ../index.php');
+}
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
