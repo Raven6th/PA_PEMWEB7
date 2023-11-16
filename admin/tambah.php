f2d66749ea49fe7891a0b5980a4d0684127378a0
Raven6th <145556185+Raven6th@users.noreply.github.com>
2023-11-16 09:12:28 +0800

    Update tambah.php

--- admin/tambah.php
+++ admin/tambah.php
@@ -57,7 +57,7 @@ while ($row = mysqli_fetch_assoc($hasil)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Product Addition</title>
     <style>
         * {
             margin: 0;
