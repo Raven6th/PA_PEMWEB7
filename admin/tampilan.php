fc515f39f93f727a9ae34e85f294b581677febdc
Raven6th <145556185+Raven6th@users.noreply.github.com>
2023-11-16 09:12:58 +0800

    Update tampilan.php

--- admin/tampilan.php
+++ admin/tampilan.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
+    <title>Product Display</title>
     <style>
         :root {
             --sidebar-color: #333;
