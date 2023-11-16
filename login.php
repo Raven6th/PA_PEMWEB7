7f26b49b80ce57ec8189067e6a1ce17ec830c854
Raven6th <145556185+Raven6th@users.noreply.github.com>
2023-11-16 09:10:08 +0800

    Update login.php

--- login.php
+++ login.php
@@ -54,7 +54,7 @@ if (isset($_POST["login"])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>Chocolate Login</title>
   <link rel="stylesheet" href="register-login.css">
 </head>
 
